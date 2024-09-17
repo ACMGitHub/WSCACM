8b131e650951eeeddf02cd5e36012aa3a2efa4ec
Brogoming <kotag@outlook.com>
2024-09-17 16:54:06 -0500

    try now

--- build/_app/immutable/chunks/paths.U1wK8Sam.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1m4m4ov)==null?void 0:s.base)??"/WSCACM";var a;const t=((a=globalThis.__sveltekit_1m4m4ov)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ build/_app/immutable/entry/start.CldSze18.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.B9xEAOCp.js";export{t as start};
