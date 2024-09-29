243873e1c4ac4feb1d8548fa1264519dc5c3c3bc
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-09-28 21:40:17 -0500

    contact cards made for officers

--- src/routes/contactus/+page.svelte
+++ src/routes/contactus/+page.svelte
@@ -5,85 +5,103 @@
 	const avatarStyle = 'w-20 rounded-full';
 </script>
 
-<div>
-	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-5xl"
-		>Contact Us</Heading
-	>
-	<div class="text-base space-y-4">
-		<p>No matter who you are, we want you to join us.</p>
-		<p>
-			As far as college clubs go, ACM is a truly special one. Our club focuses on all things
-			technology and computers, however, you don't need to be good with computers to enjoy our club.
-		</p>
-		<p>If you have any questions feel free to contact any of us.</p>
-	</div>
 
-	<div class="w-fit mx-auto pt-4 space-y-4">
-		<Heading tag="h1" customSize="text-xl font-extrabold  md:text-2xl lg:text-3xl"
-			>2024 Officer Team</Heading
-		>
-		<div class="flex items-center">
-			<Avatar src="{base}/images/officers/dakotaG.jpg" alt="Dakota Gullicksen" class={avatarStyle} />
-			<div class="pl-2">
-				<p class="font-bold">Dakota Gullicksen - President</p>
-				<a href="mailto:kogull01@wsc.edu">kogull01@wsc.edu</a>
-			</div>
+<div class="bg-white py-8">
+	<div class="mx-auto max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8">
+		<div class="max-w-2xl">
+			<h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">Contact Us</h2>
+			<p class="mt-6 text-lg pl-6 leading-8">
+				No matter who you are, we want you to join us.
+				As far as college clubs go, ACM is a truly special one. Our club focuses on all things
+				technology and computers, however, you don't need to be good with computers to join our club.
+				If you have any questions feel free to contact any of us.
+			</p>
 		</div>
 
-		<div class="flex items-center">
-			<Avatar src="{base}/images/officers/JaydenC.jpg" alt="Jayden Carper" class={avatarStyle} />
-			<div class="pl-2">
-				<p class="font-bold">Jayden Carper - Vice President</p>
-				<a href="mailto:jacarp01@wsc.edu">jacarp01@wsc.edu</a>
-			</div>
-		</div>
+		<p class="pt-20 font-bold text-xl md:text-3xl">2024 Ofiicer Team</p>
 
-		<div class="flex items-center">
-			<Avatar src="{base}/images/officers/Aunik.jpg" alt="Ajharul Islam Aunik" class={avatarStyle} />
-			<div class="pl-2">
-				<p class="font-bold">Ajharul Islam Aunik - Secretary</p>
-				<a href="mailto:ajauni01@wsc.edu">ajauni01@wsc.edu</a>
-			</div>
-		</div>
+		<ol class="py-4 px-4 grid gap-x-8 gap-y-12 md:grid-cols-2 lg:grid-cols-3 sm:gap-y-16">
+			<li>
+				<div class="flex items-center gap-x-6 shadow-xl flex-row p-4 my-4 rounded hover:scale-105 duration-300 ">
+					<Avatar href="https://www.linkedin.com/in/dakota-w-gullicksen/" target="_blank" src="{base}/images/officers/dakotaG.jpg" alt="Dakota Gullicksen" class={avatarStyle} />
+					<div>
+						<h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Dakota Gullicksen</h3>
+						<p class="text-sm font-semibold leading-6">President</p>
+						<a href="mailto:kogull01@wsc.edu">kogull01@wsc.edu</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="flex items-center gap-x-6 shadow-xl flex-row p-4 my-4 rounded hover:scale-105 duration-300 ">
+					<Avatar href="https://www.linkedin.com/in/jayden-carper-5a66622b1/" target="_blank" src="{base}/images/officers/JaydenC.jpg" alt="Jayden Carper" class={avatarStyle} />
+					<div>
+						<h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Jayden Carper</h3>
+						<p class="text-sm font-semibold leading-6">Vice President</p>
+						<a href="mailto:jacarp01@wsc.edu">jacarp01@wsc.edu</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="flex items-center gap-x-6 shadow-xl flex-row p-4 my-4 rounded hover:scale-105 duration-300 ">
+					<Avatar href="https://www.linkedin.com/in/ajharul-islam-aunik/" target="_blank" src="{base}/images/officers/Aunik.jpg" alt="Ajharul Islam Aunik" class={avatarStyle} />
+					<div>
+						<h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Ajharul Islam Aunik</h3>
+						<p class="text-sm font-semibold leading-6">Secretary</p>
+						<a href="mailto:ajauni01@wsc.edu">ajauni01@wsc.edu</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="flex items-center gap-x-6 shadow-xl flex-row p-4 my-4 rounded hover:scale-105 duration-300 ">
+					<Avatar href="https://www.linkedin.com/in/adah-heng/" target="_blank" src="{base}/images/officers/AdahH.jpg" alt="Adah Heng" class={avatarStyle} />
+					<div>
+						<h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Adah Heng</h3>
+						<p class="text-sm font-semibold leading-6">Treasurer</p>
+						<a href="mailto:adheng01@wsc.edu">adheng01@wsc.edu</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="flex items-center gap-x-6 shadow-xl flex-row p-4 my-4 rounded hover:scale-105 duration-300 ">
+					<Avatar href="#" src="{base}/images/officers/no-profile.png" alt="Tristin Patzel" class={avatarStyle} />
+					<div>
+						<h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Tristin Patzel</h3>
+						<p class="text-sm font-semibold leading-6">Historian</p>
+						<a href="mailto:trpatz01@wsc.edu">trpatz01@wsc.edu</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="flex items-center gap-x-6 shadow-xl flex-row p-4 my-4 rounded hover:scale-105 duration-300 ">
+					<Avatar href="https://www.linkedin.com/in/brandon-berger-ne/" target="_blank" src="{base}/images/officers/BrandonB.png" alt="Brandon Berger" class={avatarStyle} />
+					<div>
+						<h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Brandon Berger</h3>
+						<p class="text-sm font-semibold leading-6">Recruiter</p>
+						<a href="mailto:brberg05@wsc.edu">brberg05@wsc.edu</a>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="flex items-center gap-x-6 shadow-xl flex-row p-4 my-4 rounded hover:scale-105 duration-300 ">
+					<Avatar href="https://www.linkedin.com/in/jeremy-wynia-66004943/" target="_blank" src="{base}/images/officers/Jeremy_Wynia.jpg" alt="Jeremy Wynia" class={avatarStyle} />
+					<div>
+						<h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Jeremy Wynia</h3>
+						<p class="text-sm font-semibold leading-6">Advisor</p>
+						<a href="mailto:jewynia1@wsc.edu">jewynia1@wsc.edu</a>
+					</div>
+				</div>
+			</li>
 
-		<div class="flex items-center">
-			<Avatar src="{base}/images/officers/AdahH.jpg" alt="Adah Heng" class={avatarStyle} />
-			<div class="pl-2">
-				<p class="font-bold">Adah Heng - Treasurer</p>
-				<a href="mailto:adheng01@wsc.edu">adheng01@wsc.edu</a>
-			</div>
-		</div>
+			
+		</ol>
 
-		<div class="flex items-center">
-			<Avatar src="{base}/images/officers/no-profile.png" alt="Tristin Patzel" class={avatarStyle} />
-			<div class="pl-2">
-				<p class="font-bold">Tristin Patzel - Historian</p>
-				<a href="mailto:trpatz01@wsc.edu">trpatz01@wsc.edu</a>
-			</div>
-		</div>
-
-		<div class="flex items-center">
-			<Avatar src="{base}/images/officers/BrandonB.png" alt="Brandon Berger" class={avatarStyle} />
-			<div class="pl-2">
-				<p class="font-bold">Brandon Berger - Recruiter</p>
-				<a href="mailto:brberg05@wsc.edu">brberg05@wsc.edu</a>
-			</div>
-		</div>
-
-		<div class="flex items-center">
-			<Avatar src="{base}/images/officers/Jeremy_Wynia.jpg" alt="Jeremy Wynia" class={avatarStyle} />
-			<div class="pl-2">
-				<p class="font-bold">Jeremy Wynia - Advisor</p>
-				<a href="mailto:jewynia1@wsc.edu">jewynia1@wsc.edu</a>
-			</div>
-		</div>
 	</div>
 </div>
+
 
 <style>
 	a {
 		color: rgb(30 58 138);
-		text-decoration: underline;
 		font-weight: bold;
 	}
 	a:hover {
