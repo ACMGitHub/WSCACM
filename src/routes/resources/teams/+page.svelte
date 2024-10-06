5f3daf2d80f707552ed08fb19105df95b6497bcc
confidenceaffang <coaffa01@wsc.edu>
2024-10-06 16:25:04 -0500

    editted and added some pic to joining teams

--- src/routes/resources/teams/+page.svelte
+++ src/routes/resources/teams/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { Heading, Hr } from 'flowbite-svelte';
-	import pic1 from "..../static/images/teams/pic1"
 </script>
 
 <div>
@@ -23,30 +22,91 @@
 		people individually or in groups, which helps cut down the amount of emails being sent back and
 		forth.
 	</p>
-	<h2>
+	<h2 class="font-semibold text-3xl py-8">
 		Installation on PC
 	</h2>
-	<p>
+	<p class="text-2xl text-bold py-8 font-md">
 		Steps
 	</p>
-	<p>
+	<p class="py-4">
 		You can download teams by clicking the link below
-		<button class="py-4 bg-purple-800">
-			<a href="https://go.microsoft.com/fwlink/?linkid=2281613&clcid=0x409&culture=en-us&country=us">Teams Microsoft</a>
-		</button>
-		<img src={pic1} alt="Download teams" />
+		
+		
 	</p>
+	<button class="p-4 bg-gradient-to-r from-slate-100 to-purple-600 rounded-full">
+			<a href="https://www.microsoft.com/en-us/microsoft-teams/download-app" target="blank">Teams Microsoft</a>
+		</button>
+		<img src="/images/teams/teams3.png" alt="Download teams" class="py-8" width={465} />
+
+		<p>
+			Click on <b>Download Microsoft for windows</b> as in the image description below
+		</p>
+
+		<img src="/images/teams/teams1.png" alt="Download teams" class="py-8" width={465} />
+
+		<p class="py-8">
+			Log in using your Wayne State College (.edu) email, and you’ll see Microsoft Teams set up just as shown in the image below.
+		</p>
+
+		<img src="/images/teams/pic4.png" alt="Download teams" class="py-8" width={465} />
+
+		<h2 class="font-semibold text-3xl py-8">
+			Installation on A Phone
+		</h2>
+		
+		<p class="text-2xl text-bold py-8 font-md">
+			Steps
+		</p>
+		<p>
+			Open Playstore on your phone
+		</p>
+		<img src="/images/teams/onphone0.jpeg" alt="playstore" class="py-8" width={465} />
+
+		<p>
+			Search for Miscrosoft Teams in the address bar
+		</p>
+		<img src="/images/teams/onphone1.jpeg" alt="playstore" class="py-8" width={465} />
+
+		<p>
+			Click on the install to install Microsoft Teams to your phone
+		</p>
+
+		<img src="/images/teams/onphone2.jpeg" alt="playstore" class="py-8" width={465} />
+
+		<p>
+			Login using your Wayne State Collge email address (.edu) and enjoy teams
+		</p>
+		<img src="/images/teams/onphone3.avif" alt="teams" class="py-8" width={465} />
+
+		<h2 class= "py-8  text-3xl font-semibold">
+			Accessing Teams Online 
+		</h2>
+
+		<p class="py-4">
+		All Wayne State College Students have access to Teams through their mywsc. To view Teams on your
+		browser all you have to do is log into your mywsc account and click on <span class="font-bold"><a href="https://www.wsc.edu/current-students">Resources for current Students</a></span>
+		</p>
+		
+		<p class="pb-4">
+			Click on <b>Show More Applications</b>
+		</p>
+		<img src="/images/teams/wsc4.png" alt="teams" class="py-8" width={465} />
+
+		<p class="pb-4">
+			Click on <b> Microsoft Teams</b>
+		</p>
+		<img src="/images/teams/wsc5.png" alt="teams" class="py-8" width={465} />
+
+		<p>
+			Login using your Wayne State Collge email address (.edu) and enjoy teams
+		</p>
+		<img src="/images/teams/wsc3.png" alt="teams" class="py-8" width={465} />
+
 	
-	
+		
 	<p class="pb-4">
-		All Wayne State College Students have access to Teams through their myWSC. To view Teams on your
-		browser all you have to do is log into your <b>myWSC</b>, go to
-		<a href="https://www.wsc.edu/current-students">Resources for Current Students</a>, click on
-		<b>Show More Applications</b>, then scroll down until you find <b>Mircosoft Teams</b>. To
-		download Microsoft Teams click
-		<a href="https://www.microsoft.com/en-us/microsoft-teams/log-in">here</a>. Make sure you sign in
-		with your Wayne State College account. For more information about Microsoft Teams click
-		<a href="https://www.wsc.edu/directory/31/help_sheets/category/957/categoryInfo/35">here</a>.
+    	For more information about Microsoft Teams click
+		<a href="https://www.wsc.edu/directory/31/help_sheets/category/957/categoryInfo/35">Here</a>.
 	</p>
 </div>
 
