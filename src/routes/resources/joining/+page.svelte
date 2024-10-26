beb8a3364e646dd6ff38e401211b2bcdbfda943f
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-10-26 16:41:36 -0500

    revising how to join

--- src/routes/resources/joining/+page.svelte
+++ src/routes/resources/joining/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { A, Heading, Hr, P } from 'flowbite-svelte';
+	import { A, Heading, Hr, P, List } from 'flowbite-svelte';
+	import { base } from '$app/paths';
 </script>
 
 <div class="space-y-4">
@@ -14,7 +15,30 @@
 		towards inproving all student's education and experience, and your involvement is instrumental
 		in this endeavor.
 	</p>
+
+	<div class="py-4 grid gap-x-4 gap-y-4 md:grid-cols-2 sm:gap-y-16">
+		<div>
+			<p>Steps to join <span>WSC ACM</span>:</p>
+			<List tag="ul">
+				<li>
+					Create an account with <a href="https://accounts.acm.org/">myACM</a>: It’s free and gives you access to <span>ACM</span> resources and opportunities.
+				</li>
+				<li>
+					Send your <span>ACM ID</span> to the <span>President</span>  or <span>Vice President</span>: This allows us to add you to the <span>WSC ACM</span> chapter, to continue as a club on campus we need at least 20 registered members.
+				</li>
+				<li>
+					Attend our <span>ACM</span> meetings: Check the <a href="{base}/meetingtimes">Meeting Times</a> page for more information. If our meeting times don't work with you, you can still participate in <span>ACM</span> with our subcommittees.
+				</li>
+			</List>
+		</div>
+		<div class="flex">
+			<img src="{base}/images/ACMLogo.png" alt="Logo" height="100%" width="700">
+		</div>
+	</div>
 	<p>
+		<span>Membership dues are $20 for one school year.</span> They are not required but it does help the club afford T-shirts and equipment.
+	</p>
+	<!-- <p>
 		If you are interested in joining ACM you can simply attend one our meetings or talk to anyone
 		that you know is associated with ACM about joining. If our meeting times don't work with you,
 		you can still participate in ACM with our subcommittees.
@@ -38,5 +62,15 @@
 		Once you have successfully made an account, please forward your ACM ID number to either the
 		President or Secretary. Your contribution is pivotal in helping us achieve our membership goals
 		and further our impact as a chapter.
-	</p>
+	</p> -->
 </div>
+
+<style>
+	span {
+		font-weight: bold;
+	}
+
+	a {
+		color: blue;
+	}
+</style>
