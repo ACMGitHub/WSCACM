6e52de4a652951d1282370ed2db4d1b618ae29ef
confidenceaffang <coaffa01@wsc.edu>
2024-10-27 16:29:54 -0500

    made wsc a clickable link

--- src/routes/resources/teams/+page.svelte
+++ src/routes/resources/teams/+page.svelte
@@ -3,36 +3,50 @@
 </script>
 
 <div>
-	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-5xl text-purple-600"
+	<Heading
+		tag="h1"
+		class="mb-4"
+		customSize="text-4xl font-extrabold  md:text-5xl lg:text-5xl text-purple-600"
 		>Microsoft Teams</Heading
 	>
 	<Hr />
-	
+
 	<div class="pb-4">
-		If you are joining <b>ACM</b> it is recommended that you download <b>Teams</b> on your computer or phone as a majority of special updates and messages will presented there. All <b>Wayne State College</b> students have access to <b>Teams</b> through their <b>myWSC</b>. To have access to the <b>ACM Team</b> talk to or message an <b>ACM officer</b> to add you.
-	</div>	
+		If you are joining <b>ACM</b> it is recommended that you download <b>Teams</b> on your computer
+		or phone as a majority of special updates and messages will presented there. All
+		<b>Wayne State College</b>
+		students have access to <b>Teams</b> through their <b>myWSC</b>. To have access to the
+		<b>ACM Team</b>
+		talk to or message an <b>ACM officer</b> to add you.
+	</div>
 	<div class="flex flex-wrap">
 		<div>
 			<b>To view Teams on browser:</b>
 			<p class="pb-8">
-				1. Log into your <b>myWSC</b> account <br>
-				2. Go to <a href="https://www.wsc.edu/current-students" target="blank">Resources for Current Students</a> <br>
-				3. Click on <b>Show More Applications</b> <br>
+				1. Log into your <a href="https://www.wsc.edu/mywsc" target="blank"><b>myWSC</b></a> account
+				<br />
+				2. Go to
+				<a href="https://www.wsc.edu/current-students" target="blank"
+					>Resources for Current Students</a
+				> <br />
+				3. Click on <b>Show More Applications</b> <br />
 				4. Scroll down until you find <b>Microsoft Teams</b>
 			</p>
 			<b>To download Microsoft Teams</b>
 			<p>
-				1. Go to the <a href="https://www.microsoft.com/en-us/microsoft-teams/download-app" target="blank">Microsoft Teams download page</a> <br>
-				2. Click Download for desktop (or Download for mobile if needed) <br>
-				3. Open the downloaded file and follow the installation prompts. <br>
+				1. Go to the <a
+					href="https://www.microsoft.com/en-us/microsoft-teams/download-app"
+					target="blank">Microsoft Teams download page</a
+				> <br />
+				2. Click Download for desktop (or Download for mobile if needed) <br />
+				3. Open the downloaded file and follow the installation prompts. <br />
 				4. Launch <b>Microsoft Teams</b> and sign in with your <b>Wayne State College</b> account
 			</p>
 		</div>
-		<div>
+		<div></div>
+	</div>
+</div>
 
-		</div>
-	</div>
-	</div>
 <style>
 	a {
 		color: rgb(30 58 138);
