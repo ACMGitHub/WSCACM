08ad3e3a02c0b8af0cd2808fd245f30e287cd180
confidenceaffang <coaffa01@wsc.edu>
2024-10-27 14:16:00 -0500

    changed the text

--- src/routes/resources/teams/+page.svelte
+++ src/routes/resources/teams/+page.svelte
@@ -7,109 +7,32 @@
 		>Microsoft Teams</Heading
 	>
 	<Hr />
-	<p class="pb-4">
-		Wayne State College ACM communicates and stores files using Microsoft Teams. Once a member of
-		ACM you will have access to use the ACM Team in Teams. Teams allows us to branch out into
-		different channels so we have a conversations for said channel. Everyone has access to all of
-		the main channels except for the ACM Officers and ACM Officers Tasks channels as those are only
-		for ACM Officers. Channels to keep an eye on are <b
-			>General, Internship Opportunities, and any of the subcommittee channels</b
-		>.
-	</p>
-	<p class="pb-4">
-		If you are joining ACM it is recommended that you download Teams on your computer or phone as a
-		majority of special updates and messages will presented there. Teams also allows you to message
-		people individually or in groups, which helps cut down the amount of emails being sent back and
-		forth.
-	</p>
-	<h2 class="font-semibold text-3xl py-8">
-		Installation on PC
-	</h2>
-	<p class="text-2xl text-bold py-8 font-md">
-		Steps
-	</p>
-	<p class="py-4">
-		You can download teams by clicking the link below
-		
-		
-	</p>
-	<button class="p-4 bg-gradient-to-r from-slate-100 to-purple-600 rounded-full">
-			<a href="https://www.microsoft.com/en-us/microsoft-teams/download-app" target="blank">Teams Microsoft</a>
-		</button>
-		<img src="/images/teams/teams3.png" alt="Download teams" class="py-8" width={465} />
-
-		<p>
-			Click on <b>Download Microsoft for windows</b> as in the image description below
-		</p>
-
-		<img src="/images/teams/teams1.png" alt="Download teams" class="py-8" width={465} />
-
-		<p class="py-8">
-			Log in using your Wayne State College (.edu) email, and you’ll see Microsoft Teams set up just as shown in the image below.
-		</p>
-
-		<img src="/images/teams/pic4.png" alt="Download teams" class="py-8" width={465} />
-
-		<h2 class="font-semibold text-3xl py-8">
-			Installation on A Phone
-		</h2>
-		
-		<p class="text-2xl text-bold py-8 font-md">
-			Steps
-		</p>
-		<p>
-			Open Playstore on your phone
-		</p>
-		<img src="/images/teams/onphone0.jpeg" alt="playstore" class="py-8" width={465} />
-
-		<p>
-			Search for Miscrosoft Teams in the address bar
-		</p>
-		<img src="/images/teams/onphone1.jpeg" alt="playstore" class="py-8" width={465} />
-
-		<p>
-			Click on the install to install Microsoft Teams to your phone
-		</p>
-
-		<img src="/images/teams/onphone2.jpeg" alt="playstore" class="py-8" width={465} />
-
-		<p>
-			Login using your Wayne State Collge email address (.edu) and enjoy teams
-		</p>
-		<img src="/images/teams/onphone3.avif" alt="teams" class="py-8" width={465} />
-
-		<h2 class= "py-8  text-3xl font-semibold">
-			Accessing Teams Online 
-		</h2>
-
-		<p class="py-4">
-		All Wayne State College Students have access to Teams through their mywsc. To view Teams on your
-		browser all you have to do is log into your mywsc account and click on <span class="font-bold"><a href="https://www.wsc.edu/current-students">Resources for current Students</a></span>
-		</p>
-		
-		<p class="pb-4">
-			Click on <b>Show More Applications</b>
-		</p>
-		<img src="/images/teams/wsc4.png" alt="teams" class="py-8" width={465} />
-
-		<p class="pb-4">
-			Click on <b> Microsoft Teams</b>
-		</p>
-		<img src="/images/teams/wsc5.png" alt="teams" class="py-8" width={465} />
-
-		<p>
-			Login using your Wayne State Collge email address (.edu) and enjoy teams
-		</p>
-		<img src="/images/teams/wsc3.png" alt="teams" class="py-8" width={465} />
-
 	
-		
-	<p class="pb-4">
-    	For more information about Microsoft Teams click
-		<a href="https://www.wsc.edu/directory/31/help_sheets/category/957/categoryInfo/35">Here</a>.
-	</p>
-</div>
+	<div class="pb-4">
+		If you are joining <b>ACM</b> it is recommended that you download <b>Teams</b> on your computer or phone as a majority of special updates and messages will presented there. All <b>Wayne State College</b> students have access to <b>Teams</b> through their <b>myWSC</b>. To have access to the <b>ACM Team</b> talk to or message an <b>ACM officer</b> to add you.
+	</div>	
+	<div class="flex flex-wrap">
+		<div>
+			<b>To view Teams on browser:</b>
+			<p class="pb-8">
+				1. Log into your <b>myWSC</b> account <br>
+				2. Go to <a href="https://www.wsc.edu/current-students" target="blank">Resources for Current Students</a> <br>
+				3. Click on <b>Show More Applications</b> <br>
+				4. Scroll down until you find <b>Microsoft Teams</b>
+			</p>
+			<b>To download Microsoft Teams</b>
+			<p>
+				1. Go to the <a href="https://www.microsoft.com/en-us/microsoft-teams/download-app" target="blank">Microsoft Teams download page</a> <br>
+				2. Click Download for desktop (or Download for mobile if needed) <br>
+				3. Open the downloaded file and follow the installation prompts. <br>
+				4. Launch <b>Microsoft Teams</b> and sign in with your <b>Wayne State College</b> account
+			</p>
+		</div>
+		<div>
 
+		</div>
+	</div>
+	</div>
 <style>
 	a {
 		color: rgb(30 58 138);
