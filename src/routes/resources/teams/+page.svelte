cbc6062cf752797c5d4af6cffb59e0071f31e512
confidenceaffang <coaffa01@wsc.edu>
2024-11-10 18:05:09 -0600

    added the new gif

--- src/routes/resources/teams/+page.svelte
+++ src/routes/resources/teams/+page.svelte
@@ -1,50 +1,66 @@
 <script>
-	import { Heading, Hr } from 'flowbite-svelte';
+	import { Heading, Hr, List, Li } from 'flowbite-svelte';
+	import { base } from '$app/paths';
 </script>
 
-<div>
-	<Heading
-		tag="h1"
-		class="mb-4"
-		customSize="text-4xl font-extrabold  md:text-5xl lg:text-5xl text-purple-600"
+<div class="space-y-4">
+	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-5xl"
 		>Microsoft Teams</Heading
 	>
 	<Hr />
 
-	<div class="pb-4">
+	<p>
 		If you are joining <b>ACM</b> it is recommended that you download <b>Teams</b> on your computer
 		or phone as a majority of special updates and messages will presented there. All
 		<b>Wayne State College</b>
 		students have access to <b>Teams</b> through their <b>myWSC</b>. To have access to the
 		<b>ACM Team</b>
 		talk to or message an <b>ACM officer</b> to add you.
-	</div>
-	<div class="flex flex-wrap">
-		<div>
-			<b>To view Teams on browser:</b>
-			<p class="pb-8">
-				1. Log into your <a href="https://www.wsc.edu/mywsc" target="blank"><b>myWSC</b></a> account
-				<br />
-				2. Go to
-				<a href="https://www.wsc.edu/current-students" target="blank"
-					>Resources for Current Students</a
-				> <br />
-				3. Click on <b>Show More Applications</b> <br />
-				4. Scroll down until you find <b>Microsoft Teams</b>
-			</p>
-			<b>To download Microsoft Teams</b>
-			<p>
-				1. Go to the <a
-					href="https://www.microsoft.com/en-us/microsoft-teams/download-app"
-					target="blank">Microsoft Teams download page</a
-				> <br />
-				2. Click Download for desktop (or Download for mobile if needed) <br />
-				3. Open the downloaded file and follow the installation prompts. <br />
-				4. Launch <b>Microsoft Teams</b> and sign in with your <b>Wayne State College</b> account
-			</p>
+	</p>
+	<div class="py-4 md:flex md:justify-between">
+		<div class="md:w-1/2">
+			<p class="text-xl font-bold text-gray-900 sm:text-xl">To view Teams on browser:</p>
+			<br />
+			<List class="list-decimal space-y-2 pb-4">
+				<Li>
+					Log into your <a href="https://www.wsc.edu/mywsc" target="blank"><b>myWSC</b></a> account
+				</Li>
+				<Li>
+					Go to
+					<a href="https://www.wsc.edu/current-students" target="blank"
+						>Resources for Current Students</a
+					>
+				</Li>
+				<Li>
+					Click on <b>Show More Applications</b>
+				</Li>
+				<Li>
+					Scroll down until you find <b>Microsoft Teams</b>
+				</Li>
+			</List>
+			<p class="text-xl font-bold text-gray-900 sm:text-xl">To download Microsoft Teams:</p>
+			<br />
+			<List class="list-decimal space-y-2">
+				<Li>
+					Go to the <a
+						href="https://www.microsoft.com/en-us/microsoft-teams/download-app"
+						target="blank">Microsoft Teams download page</a
+					>
+				</Li>
+				<Li>Click Download for desktop (or Download for mobile if needed)</Li>
+				<Li>Open the downloaded file and follow the installation prompts.</Li>
+				<Li>
+					Launch <b>Microsoft Teams</b> and sign in with your <b>Wayne State College</b> account
+				</Li>
+			</List>
 		</div>
 		<div>
-			<img src="/images/teams 1.gif"/>
+			<img
+				src="{base}/images/howtojointeams.gif"
+				alt="how to join teams"
+				height="100%"
+				width="700"
+			/>
 		</div>
 	</div>
 </div>
