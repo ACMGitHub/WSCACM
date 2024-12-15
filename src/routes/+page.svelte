8b5169497a6cf5e995fbe2af5dcf03deca4c93a1
Typher7 <111155443+Typher7@users.noreply.github.com>
2024-12-15 00:01:34 -0600

    carousel styling changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,8 @@
 	import images from '../routes/images.json';
 </script>
 
-<div class="grid md:grid-cols-3 gap-8">
-	<div class="bg-[url(/images/circuit-board.jpeg)] bg-fixed bg-center my-0">
+<div class="grid md:grid-cols-3 gap-8 px-4">
+	<div class="bg-[url(/images/circuit-board.jpeg)] bg-fixed bg-center">
 		<Img src="{base}/images/ACMLogo.png" alt="ACM Banner" />
 
 		<Heading
@@ -16,20 +16,20 @@
 		</Heading>
 	</div>
 
-	<div class="md:col-span-2 justify-between px-4">
+	<div class="md:col-span-2 justify-between px-4 pb-10">
 		<div class="w-full lg:px-44">
 			<Carousel
-				class="flex h-[450px] rounded-3xl justify-center items-center"
+				class="flex h-[550px] justify-center items-center"
 				{images}
 				duration="7000"
 				let:Controls
 				let:Indicators
 			>
-				<Controls />
+				<Controls class="px-10"/>
 				<Indicators />
 			</Carousel>
 		</div>
-		<p class="text-center text-xl py-10">
+		<p class="text-center text-xl py-20">
 			WSC ACM Student Chapter offers the opportunity to meet peers and experts in many fields of
 			interest. With a world-wide membership, ACM is a leading resource for computing professional
 			and students working in various fields of information technology.
