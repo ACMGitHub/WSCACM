f5ea169840bde7736e874b7628a746ca77afd910
Enoch Atuahene <111155443+Typher7@users.noreply.github.com>
2024-12-29 17:51:09 -0600

    Home page redesign (#33)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,37 @@
 <script>
-	import { Gallery } from 'flowbite-svelte';
-
-	const images = [
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/clubFair.jpeg' },
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/bm3.jpeg' },
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/IMG_2382.jpeg' },
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/IMG_2389.jpeg' },
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/IMG_2400.jpeg' },
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/bm1.jpeg' },
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/dqcheck.jpeg' },
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/chaosturn.jpg' },
-		{ alt: 'place holder', src: 'images/none_specific/2024_2025/bm5.jpeg' }
-	];
+	import { Carousel, Img, Heading } from 'flowbite-svelte';
+	import { base } from '$app/paths';
+	import images from '../routes/images.json';
 </script>
 
-<p class="text-center text-2xl pb-8">
-	WSC ACM Student Chapter offers the opportunity to meet peers and experts in many fields of
-	interest. With a world-wide membership, ACM is a leading resource for computing professional and
-	students working in various fields of information technology.
-</p>
-<Gallery items={images} class="grid gap-4 grid-cols-3" />
+<div class="grid md:grid-cols-3 -my-8">
+	<div class="bg-[url(/images/circuit-board.jpeg)] bg-fixed bg-center flex flex-col items-center justify-center">	
+		<img src="{base}/images/ACMLogo.png" alt="ACM Banner" class='w-4/5'/>
+
+		<h2
+			class="pb-8 text-2xl font font-extrabold md:text-3xl lg:text:4xl text-slate-100 text-center"
+		>
+			Where ALL WSC Students are Welcome!
+	</h2>
+	</div>
+
+	<div class="md:col-span-2 justify-between z-0">
+		<div class="w-full lg:px-44">
+			<Carousel
+				class="flex h-[550px] justify-center items-center"
+				{images}
+				duration="7000"
+				let:Controls
+				let:Indicators
+			>
+				<Controls class="px-10" />
+				<Indicators />
+			</Carousel>
+		</div>
+		<p class="text-center text-xl py-10">
+			WSC ACM Student Chapter offers the opportunity to meet peers and experts in many fields of
+			interest. With a world-wide membership, ACM is a leading resource for computing professional
+			and students working in various fields of information technology.
+		</p>
+	</div>
+</div>
